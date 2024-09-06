949454b94c6decfd0c54c46021c09e6f97f610da
Mohankumar@LAPTOP-DSRRQ3HB <gmk9155@gmail.com>
2024-09-06 11:45:37 +0530

    hh

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import fs from "fs";
+import { format } from "date-fns";
+import path from "path";
+
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  let today = format(new Date(), "dd-mm-yyyy-HH-mm-ss");
+  res
+    .status(200)
+    .json({ message: "hai  ", timestamp: `${today}` });
+});
+
+
+app.get("/write", (req, res) => {
+
+  let today = format(new Date(), "dd-mm-yyyy-HH-mm-ss");
+
+  console.log(("today", today));
+
+
+  const filePath = `Timestamp/${today}.txt`;
+
+  fs.writeFileSync(filePath, `${today}`, "utf8");
+  
+  res.status(200).json({ message: "hai data writen" });
+  
+});
+
+
+app.get("/read", (req, res) => {
+
+
+  
+
+  const filePath = "Timestamp";
+
+
+  
+  fs.readdir(filePath, (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("error on reading files");
+    } else {
+      const textFiles = files.filter((file) => path.extname(file) === '.txt');
+      res.status(200).json(textFiles);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running in the port: ${PORT}`);
+});
+
